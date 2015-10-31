8ade77404268455a9110c1754e42504037f6610d
Missy Bur <missybur@gmail.com>
2015-10-31 08:52:17 -0700

    Add changes to towers of hanoi game

--- main.js
+++ main.js
@@ -1,73 +1,40 @@
-'use strict';
-
 $(document).ready(init);
 
 function init() {
-  console.log('ready!');
-//   $('#makeBox').on('click', makeBox);
-//   $('#randomBox').on('click', randomBox);
-  // $('.container-pole-left').on('click', '.box', leftPoleClicked);
-  // $('.container-pole-left').on('click', '.box', rightPoleClicked);
-  // $()
+  $('.tower').on('click', moveTo)
+  $('.tower').on('click', selectDisk)
+}
 
-$("div.el").on("click", function(){
-  var $this = $(this);
-  if ($this.hasClass("box-1-left")) {
+function moveTo(){
+  var opposingChild = $(this).find(">:first-child")
+  weight2 = parseInt(opposingChild.text())
+  console.log(weight2)
+  if(weight2 === NaN){
+    $(".clicked").appendTo($(this))
+  } else if (weight2 >= weight1){
+    $(this).prepend($(".clicked"))
+  } else if (weight1 > weight2){
+    return
+  } else {$(this).prepend($(".clicked"))}
 
-    $this.removeClass("box-1-left").addClass("box-1-middle").prependTo("#pole-middle");
+
+}
+var weight1 = ""
+var weight2 = ""
+
+function selectDisk() {
+  var firstKid = $(this).find(">:first-child")
+  weight1 = parseInt(firstKid.text())
+  console.log(weight1)
+  console.log(firstKid)
+  if (firstKid.hasClass('clicked')) {
+
+    firstKid.removeClass('clicked')
+
   } else {
 
-    $(this).removeClass("box-1-middle").addClass("box-1-left").prependTo("#pole-left");
-  }
-});
+    $(".clicked").removeClass('clicked')
+    firstKid.addClass('clicked')
 
-//   $('.container').click(makeBox);
-}
-
-// function moveBox(){
-//   var $this = $(this);
-
-//   if (!$('.selected').length || $this.children('.selected').length){
-
-//     return;
-// }
-
-  var $selected = $('.selected').detach();
-  $this.append($selected.detach().removeClass('selected'));
-}
-
-function boxClicked(event){
-  event.stopPropagation();
-  var $target = $(event.target);
-
-  var isSelected = $target.hasClass('selected');
-
-  if (isSelected){
-    $target.removeClass('selected');
-  } else {
-    $('.selected').removeClass('selected');
-    $target.addClass('selected');
   }
 }
-
-  // function moveSelected(){
-
-  //   console.log('moveSelected');
-  //   if ($(this).find(".selected").length > 0){
-
-  //   }else{detached = $('.selected').detach().removeClass('selected');
-  //       $(this).append(detached)};
-  // }
-
-
-
-// function makeBox() {
-//   var color = $('#color').val();
-//   var $box = makeColoredBox(color);
-//   $('#boxes').append($box);
-// }
-
-
-
-
-
