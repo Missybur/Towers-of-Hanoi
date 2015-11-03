8ca26f8136b9e856276202cfb3175a73c2321697
Missy Bur <Missybur@gmail.com>
2015-11-03 11:07:36 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,81 @@
 $(document).ready(init);
 
 function init() {
-  $('.tower').on('click', moveTo)
-  $('.tower').on('click', selectDisk)
+  $('.tower').on('click', decideWhatToDo)
+  console.log($(".tower").length)
+
+  // $('.tower').on('click', selectDisk)
 }
 
-function moveTo(){
-  var opposingChild = $(this).find(">:first-child")
-  weight2 = parseInt(opposingChild.text())
-  console.log(weight2)
-  if(weight2 === NaN){
-    $(".clicked").appendTo($(this))
-  } else if (weight2 >= weight1){
-    $(this).prepend($(".clicked"))
-  } else if (weight1 > weight2){
-    return
-  } else {$(this).prepend($(".clicked"))}
+function decideWhatToDo(){
+  var $itemSelected = $(this);
+  console.log(this)
+  var firstKid = $(this).find(".disk:first-child")
 
-
-}
-var weight1 = ""
-var weight2 = ""
-
-function selectDisk() {
-  var firstKid = $(this).find(">:first-child")
-  weight1 = parseInt(firstKid.text())
-  if (firstKid.hasClass('clicked')) {
-
-    firstKid.removeClass('clicked')
+  if ($(".clicked").length === 0) {
+    //SELECT
+    // This is what happens when there is not an item that is clicked:
+    // weight1 = parseInt(firstKid.text())
+    if (firstKid.hasClass('clicked')) {
+      firstKid.removeClass('clicked')
+    } else {
+      $(".clicked").removeClass('clicked')
+      firstKid.addClass('clicked')
+    }
 
   } else {
 
-    $(".clicked").removeClass('clicked')
-    firstKid.addClass('clicked')
+    // MOVE
+    // This is what happens when the item is clicked:
+    console.log("hello")
+    var opposingChild = $(this).find(".disk:first-child");
+    console.log(opposingChild);
+    console.log("move to function working!")
+    weight2 = parseInt(opposingChild.text());
+    weight1 = parseInt($(".clicked").text());
 
+    //  It is a legal move if the disk you are trying to move is smaller than the top disk on the stack
+
+    var isLegalMove = (weight1 < weight2) || ($(this).find(".diskContainer").children().length === 0);
+
+    console.log($(this).find(".diskContainer"))
+    // If the id of the one you move is smaller than the disk on the top of the stack
+
+    // move it!
+    if (isLegalMove) {
+      $(".clicked").prependTo( $(this).find(".diskContainer") );
+    }
+
+    // WIN
+
+    if ($("#diskContainer3").children().length === 3) {
+      console.log("You won!")
+      alert("You won!");
+    };
+
+
+
+    // if(weight2 === NaN){
+    //   $(".clicked").appendTo($(".diskContainer"))
+    //   console.log("weigh2 = NaN")
+    // } else if (weight2 >= weight1){
+    //   $(this).prepend($(".clicked"))
+    // } else if (weight1 > weight2){
+    //   return
+    // } else {
+    //   $(this).prepend($(".clicked"))
+    // }
+
+    // unselect
+    $(".clicked").removeClass("clicked");
   }
-}
+};
+
+var weight1 = ""
+var weight2 = ""
+// If there is anything that is "clicked", then move that selected item to "this"
+
+
+
 
 
