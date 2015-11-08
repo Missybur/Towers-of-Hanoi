59295b8d0064010dc03ebf6ad72a0fbadd99b877
Missy Bur <missybur@gmail.com>
2015-11-08 13:59:13 -0800

    Edit main.js page

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function init() {
   $('.tower').on('click', decideWhatToDo)
   console.log($(".tower").length)
 
-  // $('.tower').on('click', selectDisk)
 }
 
 function decideWhatToDo(){
@@ -13,9 +12,6 @@ function decideWhatToDo(){
   var firstKid = $(this).find(".disk:first-child")
 
   if ($(".clicked").length === 0) {
-    //SELECT
-    // This is what happens when there is not an item that is clicked:
-    // weight1 = parseInt(firstKid.text())
     if (firstKid.hasClass('clicked')) {
       firstKid.removeClass('clicked')
     } else {
@@ -25,8 +21,6 @@ function decideWhatToDo(){
 
   } else {
 
-    // MOVE
-    // This is what happens when the item is clicked:
     console.log("hello")
     var opposingChild = $(this).find(".disk:first-child");
     console.log(opposingChild);
@@ -34,37 +28,20 @@ function decideWhatToDo(){
     weight2 = parseInt(opposingChild.text());
     weight1 = parseInt($(".clicked").text());
 
-    //  It is a legal move if the disk you are trying to move is smaller than the top disk on the stack
-
     var isLegalMove = (weight1 < weight2) || ($(this).find(".diskContainer").children().length === 0);
 
     console.log($(this).find(".diskContainer"))
-    // If the id of the one you move is smaller than the disk on the top of the stack
 
-    // move it!
     if (isLegalMove) {
       $(".clicked").prependTo( $(this).find(".diskContainer") );
     }
 
-    // if(weight2 === NaN){
-    //   $(".clicked").appendTo($(".diskContainer"))
-    //   console.log("weigh2 = NaN")
-    // } else if (weight2 >= weight1){
-    //   $(this).prepend($(".clicked"))
-    // } else if (weight1 > weight2){
-    //   return
-    // } else {
-    //   $(this).prepend($(".clicked"))
-    // }
-
-    // unselect
     $(".clicked").removeClass("clicked");
   }
 };
 
 var weight1 = ""
 var weight2 = ""
-// If there is anything that is "clicked", then move that selected item to "this"
 
 
 
